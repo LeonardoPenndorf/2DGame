fc1cb905f012af19d0feebc204d59e7624a16b55
Leonardo Penndorf <Leonardo.penndorf@hotmail.de>
2024-01-27 17:11:02 +0100

    added player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,59 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // public variables
-    public float playerSpeed, jumpHeight;
-    public Rigidbody2D rb; // player rigid body
+    // serializedField variables
+    [SerializeField] float runSpeed, jumpSpeed;
 
     // private variables
+    private Rigidbody2D PlayerRigidbody;
+    private Animator PlayerAnimator;
+    private BoxCollider2D PlayerCollider;
+    private PolygonCollider2D PlayerFeet;
+
     private float xAxisInput, yAxisInput;
-    private bool isGrounded; // checks if the player is grounded
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRigidbody =GetComponent<Rigidbody2D>();
+        PlayerAnimator = GetComponent<Animator>();
+        PlayerCollider = GetComponent<BoxCollider2D>();
+        PlayerFeet = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
+
+        Jump();
+    }
+
+    private void Run()
+    {
         xAxisInput = Input.GetAxis("Horizontal"); // get horizontal axis input
+        PlayerAnimator.SetBool("IsRunning", xAxisInput != 0); // set running and idle animations
+        PlayerRigidbody.velocity = new Vector2(xAxisInput * runSpeed, PlayerRigidbody.velocity.y); // move
 
-        if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded) // can only jump when grounded
-        {
-            Jump();
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(xAxisInput * playerSpeed, rb.velocity.y); // move
-
-        if (rb.velocity.x < 0) // turn right
+        if (PlayerRigidbody.velocity.x < 0) // turn left
             transform.rotation = Quaternion.Euler(0, 180, 0);
-        else if (rb.velocity.x > 0) // turn left
+        else if (PlayerRigidbody.velocity.x > 0) // turn right
             transform.rotation = Quaternion.Euler(0, 0, 0);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground")) // player is grounded
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground")) // player no longer grounded
-        {
-            isGrounded = false;
-        }
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        bool isGrounded = PlayerFeet.IsTouchingLayers(LayerMask.GetMask("Ground")); // check if feet are touching the ground
+        PlayerAnimator.SetBool("IsGrounded", isGrounded); // toggle jumping animation
+
+        if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded) // can only jump when grounded
+        {
+            PlayerRigidbody.velocity = new Vector2(PlayerRigidbody.velocity.x, jumpSpeed); // jump
+        }
     }
 }
